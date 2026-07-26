d85a77eb280fdd9f0d41d690ab132d87800ce227
fadinalailahidayati-droid <fadinalailahidayati@students.amikom.ac.id>
2026-07-27 00:31:59 +0700

    Fix gitignore and remove venv from tracking

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# Memuat variabel dari file .env
+load_dotenv()
+
 class Config:
-    SECRET_KEY = "beaply_secret_key"
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 27091))
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
 
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""
-    MYSQL_DB = "beaply_db"
-
-    MYSQL_CURSORCLASS = "DictCursor"
+    MYSQL_CURSORCLASS = 'DictCursor'
+    MYSQL_CUSTOM_OPTIONS = {"ssl": {"ssl_mode": "REQUIRED"}}
